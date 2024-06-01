49057ff6f83428acd046295420662298ed200835
beMimg <bernardommguerreiro@gmail.com>
2024-06-01 18:15:17 +0200

    Swap images with tinified images

--- utils/allProjects.ts
+++ utils/allProjects.ts
@@ -1,11 +1,18 @@
-import xClone from "@/assets/x-clone.png";
-import messageApp from "@/assets/message-app.png";
-import movieWish from "@/assets/movie-wish.png";
 import mockUpMovieWish from "@/assets/mockup-moviewish.jpg";
 import mockUpMessageApp from "@/assets/mockup-messageapp.jpg";
 import mockUpXClone from "@/assets/mockup-xclone.jpg";
+import mockUpPmg from "@/assets/mockup-pmg.jpg";
 
 export const allProjects = [
+  {
+    image: mockUpPmg,
+    title: "PMG Solutions & Development",
+    description:
+      "X Clone is a social media platform inspired by X, designed to foster social interactions and sharing within a community. Dynamic platform for text/image posts, follows, likes, and comments. Profile management: trackslikes, posts, follows, and followers.",
+    tech: "React, JavaScript, TailwindCSS, Node.js, Express.js, MongoDB, Passport.js, Cloudinary",
+    preview: "https://x-clone-bemimg.netlify.app",
+    code: "https://github.com/beMimg/x-clone-frontend",
+  },
   {
     image: mockUpXClone,
     title: "X Clone",
