4f7b2ffb5db7d0b00927655730470cf68324cf87
beMimg <bernardommguerreiro@gmail.com>
2024-06-03 10:39:43 +0200

    added missing coma

--- utils/allProjects.ts
+++ utils/allProjects.ts
@@ -17,7 +17,7 @@ export const allProjects = [
     image: mockUpXClone,
     title: "X Clone",
     description:
-      "X Clone is a social media platform inspired by X, designed to foster social interactions and sharing within a community. It offers a dynamic environment for posting text and images, following other users, liking posts, and leaving comments. With comprehensive profile management features, users can easily track their likes, posts, follows, and followers",
+      "X Clone is a social media platform inspired by X, designed to foster social interactions and sharing within a community. It offers a dynamic environment for posting text and images, following other users, liking posts, and leaving comments. With comprehensive profile management features, users can easily track their likes, posts, follows, and followers.",
     tech: "React, JavaScript, TailwindCSS, Node.js, Express.js, MongoDB, Passport.js, Cloudinary",
     preview: "https://x-clone-bemimg.netlify.app",
     code: "https://github.com/beMimg/x-clone-frontend",
