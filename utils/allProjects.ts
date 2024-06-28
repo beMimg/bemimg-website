96a05ca0a5b02660aafba2bfabc7bf85fa3b60a9
beMimg <bernardommguerreiro@gmail.com>
2024-06-28 11:36:18 +0200

    Update TechStack of projects

--- utils/allProjects.ts
+++ utils/allProjects.ts
@@ -9,7 +9,7 @@ export const allProjects = [
     title: "PMG Solutions & Development",
     description:
       "Developed a professional website for a consulting firm specializing in SAP solutions. The site comprises four main pages: Home, About, Services, and Contact. This project was focused on creating a user-friendly and informative platform to highlight the company's expertise and services in the SAP domain.",
-    tech: "React, JavaScript, TypeScript, TailwindCSS",
+    tech: "React, TypeScript, TailwindCSS",
     preview: "https://pmg-solutions.es",
     code: "https://github.com/beMimg/PMG",
   },
@@ -18,7 +18,7 @@ export const allProjects = [
     title: "X Clone",
     description:
       "X Clone is a social media platform inspired by X, designed to foster social interactions and sharing within a community. It offers a dynamic environment for posting text and images, following other users, liking posts, and leaving comments. With comprehensive profile management features, users can easily track their likes, posts, follows, and followers.",
-    tech: "React, JavaScript, TailwindCSS, Node.js, Express.js, MongoDB, Passport.js, Cloudinary",
+    tech: "React, TypeScript, TailwindCSS, Node.js, Express.js, MongoDB, Passport.js, Cloudinary",
     preview: "https://x-clone-bemimg.netlify.app",
     code: "https://github.com/beMimg/x-clone-frontend",
   },
